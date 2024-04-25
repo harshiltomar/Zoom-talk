c46b6f52cd3e6cb2b8366378519d322253b7f630
Harshil Tomar <harshiltomar20@gmail.com>
2024-04-25 15:55:07 +0530

    1.0
    store classes

---
+++ src/store/Store.ts
@@ -0,0 +1,28 @@
+export type UserId = string;
+
+export interface Chat {
+  id: string;
+  userId: UserId;
+  name: string;
+  message: string;
+  upvotes: UserId[]; // Array of who has Upvoted what
+}
+
+export abstract class Store {
+  constructor() {
+    //
+  }
+
+  initRoom(roomId: string) {
+    //
+  }
+  getChats(room: string, limit: number, offset: number) {
+    //
+  }
+  addChat(userId: UserId, name: string, room: string, message: string) {
+    //
+  }
+  upvote(userId: UserId, room: string, chatId: string) {
+    //
+  }
+}
